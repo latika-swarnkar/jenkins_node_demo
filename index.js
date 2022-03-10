75ce2573cde03b691f7334175eed06e0a40d74d4
Latika Swarnkar <latika.swarnkar@innovaccer.com>
2022-03-11 00:30:46 +0530

    added career page

--- index.js
+++ index.js
@@ -10,4 +10,9 @@ app.get('/about',(req,res)=>{
     res.send("<h1>About Node JS App</h1")
 })
 
+app.get('/career',(req,res)=>{
+    console.log("About Page logged in")
+    res.send("<h1>This is Career Page</h1")
+})
+
 app.listen(3000)
