3cc94d7e8b0dc62bbf350bfa38a5c499f389fdae
Latika Swarnkar <latika.swarnkar@innovaccer.com>
2022-03-10 15:07:25 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const app=express()
+app.get('/',(req,res)=>{
+    console.log("Index Page logged in")
+    res.send("<h1>Welcome to Node JS App........</h1")
+})
+
+app.get('/about',(req,res)=>{
+    console.log("About Page logged in")
+    res.send("<h1>About Node JS App</h1")
+})
+
+app.listen(3000)
